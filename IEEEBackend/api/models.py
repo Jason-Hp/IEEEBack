c4c07ff59d14167ffde877be4c30b80d7554b2e9
Jason-Hp <j4sonho520@gmail.com>
2025-03-22 13:38:30 +0800

    Add models for database

--- IEEEBackend/api/models.py
+++ IEEEBackend/api/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Conversation(models.Model):
+    Title = models.CharField(max_length=100)
+    
+class Message(models.Model):
+    SENT_BY_CHOICES = ['user','chatbot']
+
+    conversation = models.ForeignKey(Conversation, on_delete=models.CASCADE, related_name='messages')  
+    message = models.TextField()
+    time = models.DateTimeField(auto_now_add=True)
+    sent_by = models.CharField(max_length=7, choices=SENT_BY_CHOICES)
+
+class Users(models.Model):
+    Username = models.CharField(max_length=25)
+
